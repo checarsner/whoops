d96bcbab37dd45890cd626e64830af86d55a5819
Che Carsner <checarsner@gmail.com>
2025-12-11 18:30:35 -0800

    initial commit

---
+++ src/app/app.config.ts
@@ -0,0 +1,28 @@
+import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core'
+import { provideRouter } from '@angular/router'
+import { provideStore } from '@ngrx/store'
+import { provideEffects } from '@ngrx/effects'
+import { provideStoreDevtools } from '@ngrx/store-devtools'
+import { provideRouterStore, routerReducer } from '@ngrx/router-store'
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app'
+import { provideAuth, getAuth } from '@angular/fire/auth'
+import { provideFirestore, getFirestore } from '@angular/fire/firestore'
+
+import { routes } from './app.routes'
+import { environment } from '../environment/environment'
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideRouter(routes),
+    provideStore({
+      router: routerReducer
+    }),
+    provideEffects([]),
+    provideStoreDevtools(),
+    provideRouterStore(),
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore())
+  ]
+}
---
+++ src/environment/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+    production: false,
+    firebase: {
+        apiKey: "AIzaSyBFowNLpDl7TuUSJT9l_KevaRJfxH-i3jA",
+        authDomain: "advancedlist-8255c.firebaseapp.com",
+        projectId: "advancedlist-8255c",
+        storageBucket: "advancedlist-8255c.firebasestorage.app",
+        messagingSenderId: "822866505360",
+        appId: "1:822866505360:web:c100a75f4b97c2eafcf215",
+        measurementId: "G-H8HQ0LV7LF"
+    }
+}
+
